3b6f41078385880db92265f906b6470806129362
Garaisourav12 <garaisourav12@gmail.com>
2024-09-13 15:18:22 +0530

    Message added to wrong message box - fixed

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -5,33 +5,36 @@ import Message from "./Message";
 import { addMessage, setMessages } from "../redux/slices/messagesSlice";
 
 function Messages({ socket }) {
-    const dispatch = useDispatch();
-    const selectedUser = useSelector((state) => state.selectedUser);
-    const messages = useSelector((state) => state.messages);
-    const { messages: retrievedMessages, loading } =
-        useGetMessages(selectedUser);
+	const dispatch = useDispatch();
+	const selectedUser = useSelector((state) => state.selectedUser);
+	const messages = useSelector((state) => state.messages);
+	const { messages: retrievedMessages, loading } =
+		useGetMessages(selectedUser);
 
-    useEffect(() => {
-        dispatch(setMessages(retrievedMessages));
-    }, [retrievedMessages]);
+	useEffect(() => {
+		dispatch(setMessages(retrievedMessages));
+	}, [retrievedMessages]);
 
-    useEffect(() => {
-        socket.on("newMessage", (data) => {
-            dispatch(addMessage(data));
-        });
-    }, []);
+	useEffect(() => {
+		socket.on("newMessage", (data) => {
+			if (data.receiverId === selectedUser._id) {
+				// Message from selected user
+				dispatch(addMessage(data));
+			}
+		});
+	}, []);
 
-    return (
-        <div className="flex-1 flex flex-col gap-2 py-4 px-3 mx-1 overflow-y-auto">
-            {!loading && (
-                <>
-                    {messages?.map((message) => (
-                        <Message key={message._id} message={message} />
-                    ))}
-                </>
-            )}
-        </div>
-    );
+	return (
+		<div className="flex-1 flex flex-col gap-2 py-4 px-3 mx-1 overflow-y-auto">
+			{!loading && (
+				<>
+					{messages?.map((message) => (
+						<Message key={message._id} message={message} />
+					))}
+				</>
+			)}
+		</div>
+	);
 }
 
 export default Messages;
